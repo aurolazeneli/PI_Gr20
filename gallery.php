340a793a75ad39b49b69c7ddf3bdb73be930aecc
BleonaKonjuhi <52915332+BleonaKonjuhi@users.noreply.github.com>
2020-05-10 01:48:44 +0200

    Commited gallery page

---
+++ gallery.php
@@ -0,0 +1,161 @@
+<html lang="en">
+<head>
+    <title>
+
+    </title>
+  <link rel="stylesheet" href="gallery.css"/>
+
+</head>
+<body>
+    <div class="banner1">
+        <img src="ph1.png" class="sliderimg" />
+        <div class="text-box">
+            <h1>Travel is everything</h1>
+            <span></span>
+            <p>
+                Duis mollis, est non commodo luctus,
+                nisi erat porttitor ligula, eget lacinia
+                odio sem nec elit. Cras mattis consectetur p
+                urus sit amet fermentum. Fusce dapibus,
+            </p>
+        </div>
+    </div>
+    <div class="banner2">
+        <img src="ph2.png" class="sliderimg" />
+        <div class="text-box">
+            <h1>Travel is everything</h1>
+            <span></span>
+            <p>
+                Duis mollis, est non commodo luctus,
+                nisi erat porttitor ligula, eget lacinia
+                odio sem nec elit. Cras mattis consectetur p
+                urus sit amet fermentum. Fusce dapibus,
+            </p>
+        </div>
+    </div>
+    <div class="banner3">
+        <img src="ph3.png" class="sliderimg" />
+        <div class="text-box">
+            <h1>Travel is everything</h1>
+            <span></span>
+            <p>
+                Duis mollis, est non commodo luctus,
+                nisi erat porttitor ligula, eget lacinia
+                odio sem nec elit. Cras mattis consectetur p
+                urus sit amet fermentum. Fusce dapibus,
+            </p>
+        </div>
+    </div>
+    <div class="banner4">
+        <img src="ph4.png" class="sliderimg" />
+        <div class="text-box">
+            <h1>Travel is everything</h1>
+            <span></span>
+            <p>
+                Duis mollis, est non commodo luctus,
+                nisi erat porttitor ligula, eget lacinia
+                odio sem nec elit. Cras mattis consectetur p
+                urus sit amet fermentum. Fusce dapibus,
+            </p>
+        </div>
+    </div>
+   
+    <div class="container">
+        <div class="image-gallery">
+            <a href="img1.png" class="img-1">
+                <i class="icon ion-md-expand"></i>
+            </a>
+            <a href="img2.png" class="img-2">
+                <i class="icon ion-md-expand"></i>
+            </a>
+            <a href="img3.png" class="img-3">
+                <i class="icon ion-md-expand"></i>
+            </a>
+            <a href="img4.png" class="img-4">
+                <i class="icon ion-md-expand"></i>
+            </a>
+            <a href="img5.png" class="img-5">
+                <i class="icon ion-md-expand"></i>
+            </a>
+            <a href="img6.png" class="img-6">
+                <i class="icon ion-md-expand"></i>
+            </a>
+            <a href="img7.png" class="img-7">
+                <i class="icon ion-md-expand"></i>
+            </a>
+            <a href="img8.png" class="img-8">
+                <i class="icon ion-md-expand"></i>
+            </a>
+        </div>
+
+    </div>
+    <div class="container1">
+        <div class="box">
+            <div class="content">
+                <h2>01</h2>
+                <h3>Service one</h3>
+                <p>
+                    Lorem Ipsum is simply
+                    dummy text of the printing
+                    and typesetting industry. Lorem Ipsum
+                    has been the industry's standard dummy
+
+                </p>
+                <a href="#">Read More</a>
+
+            </div>
+
+        </div>
+        <div class="box">
+            <div class="content">
+                <h2>01</h2>
+                <h3>Service one</h3>
+                <p>
+                    Lorem Ipsum is simply
+                    dummy text of the printing
+                    and typesetting industry. Lorem Ipsum
+                    has been the industry's standard dummy
+
+                </p>
+                <a href="#">Read More</a>
+            </div>
+
+        </div>
+        <div class="box">
+            <div class="content">
+                <h2>01</h2>
+                <h3>Service one</h3>
+                <p>
+                    Lorem Ipsum is simply
+                    dummy text of the printing
+                    and typesetting industry. Lorem Ipsum
+                    has been the industry's standard dummy
+
+                </p>
+                <a href="#">Read More</a>
+            </div>
+
+        </div>
+        <div class="box">
+            <div class="content">
+                <h2>01</h2>
+                <h3>Service one</h3>
+                <p>
+                    Lorem Ipsum is simply
+                    dummy text of the printing
+                    and typesetting industry. Lorem Ipsum
+                    has been the industry's standard dummy
+
+                </p>
+                <a href="#">Read More</a>
+            </div>
+
+        </div>
+
+    </div>
+ 
+    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+</body>
+
+
+</html>
