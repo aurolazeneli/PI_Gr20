64506fe8a4e1b4a1046802d8a4748b1567ffe830
Aurola Zeneli <52915274+aurolazeneli@users.noreply.github.com>
2020-05-11 09:01:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -30,45 +30,34 @@
     </form>
     <hr color="white" style=" margin-left:-7%;height:0.001px; width:90%">
 
-  <div class="navbar-nav">
-         <a  class="nav-item nav-link " href="#">Home </a>
-         <a class="nav-item nav-link" href="#">Hotels</a>
-         <a class="nav-item nav-link" href="#">Flights</a>
-         <a class="nav-item nav-link " href="#">Gallery</a>
-         <a class="nav-item nav-link " href="#">Contact</a>
-         <i style="color:red; size:120%; margin-left:20%;"class="fa fa-user-circle"></i>
-         <a href="#" class="nav-item nav-link"
-         <h8 style="color:white; margin:0; padding:0" >Login</h8></a>
-    <!--  <a><i style="color:red; margin-left:20%;" class="fa fa-user-circle-o"></i></a>-->
-     </div>
-
+<?php include "includes/header.php" ?>
 
      <div style="margin-top:30%" class="form-php">
-       <form>
+       <form  action="search.php" method="post">
   <div class="form-row align-items-center">
-    <div class="col-sm-3 my-1">
+  <div class="form-group col-md-4" style="margin-bottom:2px !important;">
       <label class="sr-only" for="inlineFormInputName">Where</label>
+       <select id="inputState" name="location" class="form-control">
+         <option selected>Where</option>
+         <option>Prishtina</option>
+          <option>London</option>
+           <option>Rome</option>
+            <option>Paris</option>
+             <option>Istanbul</option>
+       </select>
 
-      <input type="text" class="form-control" id="inlineFormInputName" placeholder="Jane Doe">
     </div>
     <div class="col-md-2 my-1">
-      <label class="sr-only" for="inlineFormInputName">Where</label>
 
-      <input type="text" style="width: 130px !important; " class="form-control" id="inlineFormInputName" placeholder="Jane Doe">
+        <input type="date" style="width: 130px !important; " name="start_date" class="form-control" id="inlineFormInputName" placeholder="d">
     </div>
     <div class="col-md-2 my-1">
-      <label class="sr-only" for="inlineFormInputName">Where</label>
 
-      <input type="text"style="width: 130px !important; " class="form-control" id="inlineFormInputName" placeholder="Jane Doe">
+      <input type="date" style="width: 130px !important; " name="end_date" class="form-control" id="inlineFormInputName" placeholder="d">
     </div>
-      <div class="col-md-2 my-1">
-        <label class="sr-only" for="inlineFormInputName">Where</label>
-
-        <input type="text"style="width: 130px !important; " class="form-control" id="inlineFormInputName" placeholder="Jane Doe">
-      </div>
 
     <div class="col-auto my-1">
-      <button type="submit" style="background-color:red; outline:none !important; " class="btn btn-primary">Search</button>
+      <button type="submit" style="background-color:red; outline:none !important;" name="submit" class="btn btn-primary">Search</button>
     </div>
   </div>
 </form>
@@ -76,30 +65,24 @@
      </div>
 </nav>
   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-  </ol>
+      <ol class="carousel-indicators">
+    <?php for ($i=0; $i <4 ; $i++) {
+      echo "<li data-target=\"#carouselExampleIndicators\" data-slide-to=\"$i\"></li>";
+    } ?>  </ol>
+
+
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="images/finland.png" class="d-block w-100" alt="finland">
     </div>
-    <div class="carousel-item">
-      <img src="images/greece1.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="images/maldiv.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="images/rome.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="images/swiss.png" class="d-block w-100" alt="...">
-    </div>
+<?php $photos = array("images/greece1.jpg","images/maldiv.jpg","images/swiss.png" );
+       for ($i=0; $i <3; $i++) {
+        echo "<div class=\"carousel-item\">
+          <img src=\"$photos[$i]\" class=\"d-block w-100\" alt=\"finland\">
+        </div>";
+       } ?>
   </div>
+
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
@@ -110,6 +93,7 @@
   </a>
 </div>
 </header>
+
 <section class="qualities">
   <div class="row">
   <div class="col">  <i class="fa fa-check-circle" style="font-size:48px;color:#2eb857"></i>
@@ -119,12 +103,11 @@
    <i class="fa fa-info-circle" style="font-size:48px;color:#40b3d6"></i>
     <h5>Last Minute Deals</h5>
     <p>Looking for a hotel for tonight? You can stay at the best price by following our last minute discounts instead of getting lost among hundreds of options.</p></div>
-
     <div class="col"><div class="fo"  style="background-color:#ebcd0c" >  <i class="fa fa-comments" aria-hidden="true" style="color:white; margin-top:10px; font-size: 1.3rem;" ></i></div>
       <h5>7/24 Support</h5>
       <p>By using our Call Center and online support channels, you can ask us 24/7 questions and get service on any subject you want.</p></div></div>
-
 </section>
+
 <section style="margin-bottom:50px">
   <h5 style="text-align:center">SPECIAL OFFERS</h5>
   <div  class="row">
@@ -259,21 +242,18 @@
 </div>
 
 </section>
+
 <section style="margin-top: 40px" class="tips">
   <h5 style="text-align:center; style="margin-bottom: 10px" ">FEATURED TIPS</h5>
   <table class="table table-borderless">
-
     <tbody>
       <tr>
-
         <td> <img src="images/maldiv.jpg" alt="Snow" style="width:100%"></td>
         <td> <img src="images/maldiv.jpg" alt="Snow" style="width:100%"></td>
         <td> <img src="images/maldiv.jpg" alt="Snow" style="width:100%"></td>
-
       </tr>
       <tr>
-
-<td></td>
+         <td></td>
         <td><img src="images/maldiv.jpg" alt="Snow" style="width:100%"></td>
         <td><img src="images/maldiv.jpg" alt="Snow" style="width:100%"></td>
           <td> <img src="images/maldiv.jpg" alt="Snow" style="width:100%"></td>
@@ -281,92 +261,24 @@
       <tr>  <td colspan="2"><img src="images/maldiv.jpg" alt="Snow" style="width:100%"></td>
         <th scope="row"><img src="images/maldiv.jpg" alt="Snow" style="width:100%"></th>
           <td> <img src="images/maldiv.jpg" alt="Snow" style="width:100%"></td>
-
-
       </tr>
     </tbody>
   </table>
 </section>
+
 <hr style="margin-top:40px">
+
 <section style=" margin-top:50px; margin-bottom: 60px">
   <h3 style="color:#120f57; text-align: center">Don't let our campaigns escape!</h3>
   <p  style="color:#120f57; text-align: center; width:80%; margin:0 auto">Don't decide without having to look at our easy booking campaigns that we have updated for you. With your early bookings you can plan your holiday at very affordable rates and your time and money will be in your pocket!</p>
-  <div class="row2" style="width: 95%;margin-left: 2%; margin-top:50px">
-  <div class="column2">
-    <img  src="images/rome.jpg" alt="Snow" style="width:100%; ">
-  </div>
-  <div class="column2">
-    <img src="images/rome.jpg" alt="Forest" style="width:100%">
-  </div>
-  <div class="column2">
-    <img src="images/rome.jpg" alt="Mountains" style="width:100%">
-  </div>
-  <div class="column2">
-    <img src="images/rome.jpg" alt="Mountains" style="width:100%">
-  </div>
-  <div class="column2">
-    <img src="images/rome.jpg" alt="Mountains" style="width:100%">
-  </div>
-  <div class="column2">
-    <img src="images/rome.jpg" alt="Mountains" style="width:100%">
-  </div>
 
+  <div class="row2" style="width: 95%;margin-left: 2%; margin-top:50px">
+    <?php $photos1 = array("images/rome.jpg","images/rome.jpg","images/rome.jpg","images/rome.jpg","images/rome.jpg","images/rome.jpg" );
+           for ($i=0; $i <5; $i++) {
+            echo "  <div class=\"column2\">
+                <img  src=\"$photos1[$i]\"  style=\"width:100%; \">
+              </div>";
+           } ?>
 </div>
 </section>
-<footer>
-  <div class="foot" style="background-color:#090736; width: 100%; padding-top:7px; padding-left: 20%">
-  <p style="color: white; padding-top: 5px;padding-bottom: 5px; font-size: 0.8rem; text-align: center">  You can be informed of all our opportunities! </p>
-  <div class="input-group mb-3" style="width: 360px; padding-left: 5px;">
-  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-  <div class="input-group-append">
-    <span class="input-group-text" id="basic-addon2">Sign Up!</span>
-  </div>
-</div>
-
-  </div>
-  <div class="row3">
-    <div class="column3">
-    <p>  © 2018 THE COPYRIGHT OF ALL THE
-      VISUAL MATERIALS AND ALL CONTENTS
-      IN THE SITE BELONG TO MYBOX.</p>
-    </div>
-    <div class="column3">
-    <ul>
-      <li>HOTEL</li>
-      <li>FLIGHTS</li>
-      <li>GALLERY</li>
-      <li>CONTACT</li>
-    </ol>
-    </div>
-    <div class="column3">
-<ul>
-  <li>PARIS</li>
-  <li>LONDON</li>
-  <li>MALDIVE</li>
-  <li>ROME</li>
-</ol>
-    </div>
-    <div class="column3">
-    <ul>
-      <li>SERVICE</li>
-      <li>SERVICE</li>
-      <li>SERVICE</li>
-      <li>SERVOCE</li>
-    </ol>
-    </div>
-
-  </div>
-
-  <div class="last" >
-
-   <a href="index.php" >   <button type="button" name="button" id="up" style=" background-color:#616161;"><i style="color:white;" class="fa fa-arrow-up"></i></button></a>
-  </div>
-</footer>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
-</html>
+<?php include "includes/footer.php" ?>
