88f7ac1a2e58a98e6c461b883edea756afb6dafd
BleonaKonjuhi <52915332+BleonaKonjuhi@users.noreply.github.com>
2020-05-11 14:56:44 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,14 +1,22 @@
 <html lang="en">
 <head>
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <title>
-
+        Gallery page
     </title>
-  <link rel="stylesheet" href="gallery.css"/>
+    <!--Bootstrap CSS-->
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" />
 
+  <link rel="stylesheet" href="gallery.css"/>
+   
 </head>
 <body>
+     
     <div class="banner1">
-        <img src="ph1.png" class="sliderimg" />
+        <img src="images/ph1.png" class="sliderimg" />
         <div class="text-box">
             <h1>Travel is everything</h1>
             <span></span>
@@ -21,7 +29,7 @@
         </div>
     </div>
     <div class="banner2">
-        <img src="ph2.png" class="sliderimg" />
+        <img src="images/ph2.png" class="sliderimg" />
         <div class="text-box">
             <h1>Travel is everything</h1>
             <span></span>
@@ -34,7 +42,7 @@
         </div>
     </div>
     <div class="banner3">
-        <img src="ph3.png" class="sliderimg" />
+        <img src="images/ph3.png" class="sliderimg" />
         <div class="text-box">
             <h1>Travel is everything</h1>
             <span></span>
@@ -47,7 +55,7 @@
         </div>
     </div>
     <div class="banner4">
-        <img src="ph4.png" class="sliderimg" />
+        <img src="images/ph4.png" class="sliderimg" />
         <div class="text-box">
             <h1>Travel is everything</h1>
             <span></span>
@@ -62,28 +70,28 @@
    
     <div class="container">
         <div class="image-gallery">
-            <a href="img1.png" class="img-1">
+            <a href="images/img1.png" class="img-1">
                 <i class="icon ion-md-expand"></i>
             </a>
-            <a href="img2.png" class="img-2">
+            <a href="images/img2.png" class="img-2">
                 <i class="icon ion-md-expand"></i>
             </a>
-            <a href="img3.png" class="img-3">
+            <a href="images/img3.png" class="img-3">
                 <i class="icon ion-md-expand"></i>
             </a>
-            <a href="img4.png" class="img-4">
+            <a href="images/img4.png" class="img-4">
                 <i class="icon ion-md-expand"></i>
             </a>
-            <a href="img5.png" class="img-5">
+            <a href="images/img5.png" class="img-5">
                 <i class="icon ion-md-expand"></i>
             </a>
-            <a href="img6.png" class="img-6">
+            <a href="images/img6.png" class="img-6">
                 <i class="icon ion-md-expand"></i>
             </a>
-            <a href="img7.png" class="img-7">
+            <a href="images/img7.png" class="img-7">
                 <i class="icon ion-md-expand"></i>
             </a>
-            <a href="img8.png" class="img-8">
+            <a href="images/img8.png" class="img-8">
                 <i class="icon ion-md-expand"></i>
             </a>
         </div>
@@ -153,9 +161,83 @@
         </div>
 
     </div>
+
+     
+
+      <div class="container marketing" style="margin-top:0%; padding:-10%">
+
+
+        <!-- START THE FEATURETTES -->
+
+        <hr class="featurette-divider" style="margin-bottom:-10%">
+
+        <div class="row featurette" style="padding-right:40%">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">First picture of Istambull... <span class="text-muted">It'll blow your mind.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" src="images/img7.png" alt="Generic placeholder image" width="500" height="500">
+          </div>
+        </div>
+            <hr class="featurette-divider" style="margin-bottom:-10%">
+
+        <div class="row featurette" style="padding-left:40%">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">Second picture is NEW YORK... <span class="text-muted">It'll blow your mind.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" src="images/img8.png" alt="Generic placeholder image" width="500" height="500">
+          </div>
+        </div>
+            <hr class="featurette-divider" style="margin-bottom:-10%">
+
+        <div class="row featurette" style="padding-right:40%">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">Third picture is VEGAS... <span class="text-muted">It'll blow your mind.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" src="images/img6.png" alt="Generic placeholder image" width="500" height="500">
+          </div>
+        </div>
+
+
+        <hr class="featurette-divider" style="margin-bottom:-10%">
+
+        <div class="row featurette"  style="padding-left:40%">
+          <div class="col-md-7 order-md-2">
+            <h2 class="featurette-heading">Oh yeah, DUBAI... <span class="text-muted">See for yourself.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <img class="featurette-image img-fluid mx-auto" src="images/img5.png" alt="Generic placeholder image" width="500" height="500">
+          </div>
+        </div>
+
+        <hr class="featurette-divider" style="margin-bottom:-10%">
+
+        <div class="row featurette" style="padding-right:40%">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">And lastly, this one MALDIVES... <span class="text-muted">Checkmate.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" src="images/img4.png" alt="Generic placeholder image" width="500" height="500">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <!-- /END THE FEATURETTES -->
+
+      </div><!-- /.container -->
+
  
     <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
-
 
 </html>
