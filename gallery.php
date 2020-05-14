c18b3eff5c18cb8d447669da89151ddeedc668ea
BleonaKonjuhi <52915332+BleonaKonjuhi@users.noreply.github.com>
2020-05-14 23:24:19 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,17 +1,22 @@
 <html lang="en">
 <head>
+
      <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+
     <title>
         Gallery page
     </title>
     <!--Bootstrap CSS-->
+    	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
   <link rel="stylesheet" href="gallery.css"/>
-       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    
 </head>
 <body>
@@ -98,147 +103,200 @@
         </div>
 
     </div>
-    <div class="container1">
-        <div class="box">
-            <div class="content">
-                <h2>01</h2>
-                <h3>Service one</h3>
-                <p>
-                    Lorem Ipsum is simply
-                    dummy text of the printing
-                    and typesetting industry. Lorem Ipsum
-                    has been the industry's standard dummy
-
-                </p>
-                <a href="#">Read More</a>
-
+   
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+<div class="container" style="margin-top:-10%">
+   <h3 class="pb-3 mb-4 font-italic border-bottom">
+    States
+   </h3>
+   <div class="row">
+      <div class="col-md-6">
+         <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+               <strong class="d-inline-block mb-2 text-primary" style="color:red !important";>Barcelona</strong>
+               <h6 class="mb-0">
+                  <a class="text-dark"  href="#">Barcelona city</a>
+               </h6>
+               <div class="mb-1 text-muted small">May 18</div>
+               <p class="card-text mb-auto">Founded as a Roman city, in the Middle Ages Barcelona became the capital of the County of Barcelona. .</p>
+               <a class="btn btn-outline-primary btn-sm" role="button" style="color:red !important"; href="https://en.wikipedia.org/wiki/Barcelona">Continue reading</a>
             </div>
-
-        </div>
-        <div class="box">
-            <div class="content">
-                <h2>01</h2>
-                <h3>Service one</h3>
-                <p>
-                    Lorem Ipsum is simply
-                    dummy text of the printing
-                    and typesetting industry. Lorem Ipsum
-                    has been the industry's standard dummy
-
-                </p>
-                <a href="#">Read More</a>
+            <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="images/barc.png" style="width: 200px; height: 250px;">
+         </div>
+      </div>
+      <div class="col-md-6">
+         <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+               <strong class="d-inline-block mb-2 text-success" style="color:#141d27 !important";>Berlin</strong>
+               <h6 class="mb-0">
+                  <a class="text-dark" href="#">Berlin ciyty</a>
+               </h6>
+               <div class="mb-1 text-muted small">May 18</div>
+               <p class="card-text mb-auto">Berlin straddles the banks of the River Spree, which flows into the River Havel (a tributary of the River Elbe) in the western borough of Spandau. .</p>
+               <a class="btn btn-outline-success btn-sm" href="https://en.wikipedia.org/wiki/Berlin" style="color:#141d27 !important";>Continue reading</a>
             </div>
-
-        </div>
-        <div class="box">
-            <div class="content">
-                <h2>01</h2>
-                <h3>Service one</h3>
-                <p>
-                    Lorem Ipsum is simply
-                    dummy text of the printing
-                    and typesetting industry. Lorem Ipsum
-                    has been the industry's standard dummy
-
-                </p>
-                <a href="#">Read More</a>
+            <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="images/berlin.png" style="width: 200px; height: 250px;">
+         </div>
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-md-6">
+         <div class="card border-primary flex-md-row mb-4 shadow-sm h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+               <strong class="d-inline-block mb-2 text-primary" style="color:red !important"; >Greece</strong>
+               <h6 class="mb-0">
+                  <a class="text-dark" href="#">Greece city</a>
+               </h6>
+               <div class="mb-1 text-muted small">May 18</div>
+               <p class="card-text mb-auto">Situated on the southern tip of the Balkan Peninsula, Greece is located at the crossroads of Europe, Asia, and Africa. .</p>
+               <a class="btn btn-outline-primary btn-sm" role="button" style="color:red !important"; href="https://en.wikipedia.org/wiki/Greece" >Continue reading</a>
             </div>
-
-        </div>
-        <div class="box">
-            <div class="content">
-                <h2>01</h2>
-                <h3>Service one</h3>
-                <p>
-                    Lorem Ipsum is simply
-                    dummy text of the printing
-                    and typesetting industry. Lorem Ipsum
-                    has been the industry's standard dummy
-
-                </p>
-                <a href="#">Read More</a>
+            <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="images/img3.png" style="width: 200px; height: 250px;">
+         </div>
+      </div>
+      <div class="col-md-6">
+         <div class="card border-success flex-md-row mb-4 shadow-sm h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+               <strong class="d-inline-block mb-2 text-success" style="color:#141d27 !important";>Maldives</strong>
+               <h6 class="mb-0">
+                  <a class="text-dark" href="#">Maldives city</a>
+               </h6>
+               <div class="mb-1 text-muted small">Nov 11</div>
+               <p class="card-text mb-auto">The Maldives archipelago is located on the Chagos-Laccadive Ridge, a vast submarine mountain range in the Indian Ocean; </p>
+               <a class="btn btn-outline-success btn-sm" href="https://en.wikipedia.org/wiki/Maldives"  style="color:#141d27 !important";>Continue reading</a>
             </div>
-
-        </div>
-
-    </div>
-
-     
-
-      <div class="container marketing" style="margin-top:0%; padding:-10%">
-
-
-        <!-- START THE FEATURETTES -->
-
-        <hr class="featurette-divider" style="margin-bottom:-10%">
-
-        <div class="row featurette" style="padding-right:40%">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">First picture of Istambull... <span class="text-muted">It'll blow your mind.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" src="images/img7.png" alt="Generic placeholder image" width="500" height="500">
-          </div>
-        </div>
-            <hr class="featurette-divider" style="margin-bottom:-10%">
-
-        <div class="row featurette" style="padding-left:40%">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">Second picture is NEW YORK... <span class="text-muted">It'll blow your mind.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" src="images/img8.png" alt="Generic placeholder image" width="500" height="500">
-          </div>
-        </div>
-            <hr class="featurette-divider" style="margin-bottom:-10%">
-
-        <div class="row featurette" style="padding-right:40%">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">Third picture is VEGAS... <span class="text-muted">It'll blow your mind.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" src="images/img6.png" alt="Generic placeholder image" width="500" height="500">
-          </div>
-        </div>
-
-
-        <hr class="featurette-divider" style="margin-bottom:-10%">
-
-        <div class="row featurette"  style="padding-left:40%">
-          <div class="col-md-7 order-md-2">
-            <h2 class="featurette-heading">Oh yeah, DUBAI... <span class="text-muted">See for yourself.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5 order-md-1">
-            <img class="featurette-image img-fluid mx-auto" src="images/img5.png" alt="Generic placeholder image" width="500" height="500">
-          </div>
-        </div>
-
-        <hr class="featurette-divider" style="margin-bottom:-10%">
-
-        <div class="row featurette" style="padding-right:40%">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">And lastly, this one MALDIVES... <span class="text-muted">Checkmate.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" src="images/img4.png" alt="Generic placeholder image" width="500" height="500">
-          </div>
-        </div>
-
-        <hr class="featurette-divider">
-
-        <!-- /END THE FEATURETTES -->
-
-      </div><!-- /.container -->
+            <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="images/img4.png" style="width: 200px; height: 250px;">
+         </div>
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-md-6" >
+         <div class="card text-white bg-primary flex-md-row mb-4 shadow-sm h-md-250"  style="background-color:red !important";>
+            <div class="card-body d-flex flex-column align-items-start">
+               <strong class="d-inline-block mb-2 text-white">World</strong>
+               <h6 class="mb-0">
+                  <a class="text-white" href="#">World Tour</a>
+               </h6>
+               <div class="mb-1 text-white-50 small">Nov 12</div>
+               <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+               <a class="btn btn-outline-light btn-sm" role="button" href="http://www.jquery2dotnet.com/">Continue reading</a>
+            </div>
+            <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="//placeimg.com/250/250/arch" style="width: 200px; height: 250px;">
+         </div>
+      </div>
+      <div class="col-md-6">
+         <div class="card text-white bg-success flex-md-row mb-4 shadow-sm h-md-250"  style="background-color:#141d27 !important";>
+            <div class="card-body d-flex flex-column align-items-start">
+               <strong class="d-inline-block mb-2 text-white">World Tour</strong>
+               <h6 class="mb-0">
+                  <a class="text-white" href="#">World Tour</a>
+               </h6>
+               <div class="mb-1 text-white-50 small">Nov 11</div>
+               <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+               <a class="btn btn-outline-light btn-sm" href="http://www.jquery2dotnet.com/">Continue reading</a>
+            </div>
+            <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="//placeimg.com/250/250/nature" style="width: 200px; height: 250px;">
+         </div>
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-md-4">
+         <div class="card mb-4">
+            <img class="card-img-top" src="images/london.png" alt="Card image cap">
+            <div class="card-body">
+               <h5 class="card-title">London</h5>
+               <p class="card-text">London has a diverse range of people and cultures, and more than 300 languages are spoken in the region.</p>
+               <a href="http://www.jquery2dotnet.com/" class="btn btn-outline-dark btn-sm">Go somewhere</a>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-4">
+         <div class="card mb-4 border-dark">
+            <img class="card-img-top" src="images/rome.jpg" alt="Card image cap">
+            <div class="card-body">
+               <h5 class="card-title">Rome</h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <a href="http://www.jquery2dotnet.com/" class="btn btn-dark btn-sm">Go somewhere</a>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-4">
+         <div class="card mb-4 text-white bg-dark">
+            <img class="card-img-top" src="images/stone.jpg" alt="Card image cap">
+            <div class="card-body">
+               <h5 class="card-title">Stonee</h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <a href="http://www.jquery2dotnet.com/" class="btn btn-outline-light btn-sm">Go somewhere</a>
+            </div>
+         </div>
+      </div>
+   </div>
+   <h3 class="mt-3 pb-3 mb-4 font-italic border-bottom">
+    
+   </h3>
+   <div class="row">
+      <div class="col-md-8">
+         <div class="card mb-4 bg-dark text-white">
+            <img class="card-img" src="images/img7.png" alt="Card image">
+            <div class="card-img-overlay">
+               <h5 class="card-title">Instambul</h5>
+               <p class="card-text"><?php  include 'writefile.php'?>.</p>
+               <p class="card-text">Last updated 3 mins ago</p>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-4">
+         <div class="card mb-4 bg-dark text-white">
+            <img class="card-img" src="images/img8.png" alt="Card image">
+            <div class="card-img-overlay">
+               <h5 class="card-title">Card title</h5>
+               <p class="card-text"><?php  include 'readfile.php'?>.</p>
+               <p class="card-text">Last updated 3 mins ago</p>
+            </div>
+         </div>
+      </div>
+   </div>
+  
+   <h3 class="mt-3 pb-3 mb-4 font-italic border-bottom">
+ 
+   </h3>
+   <div class="row">
+      <div class="col-md-4">
+         <div class="card">
+            <img class="card-img-top" src="images/img5.png" alt="Card image cap">
+            <div class="card-body">
+               <h5 class="card-title border-bottom pb-3">Dubai <a href="#" class="float-right d-inline-flex share"><i class="fas fa-share-alt text-primary"></i></a></h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <a href="#" class="float-right"  style="color:red !important";>Read more <i class="fas fa-angle-double-right"></i></a>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-4">
+         <div class="card">
+            <img class="card-img-top" src="images/img6.png" alt="Card image cap">
+            <div class="card-body">
+               <h5 class="card-title border-bottom pb-3">London <a href="#" class="float-right btn btn-sm btn-info d-inline-flex share"><i class="fas fa-share-alt"></i></a></h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <a href="#" class="btn btn-sm btn-info float-right"  style="background-color:#141d27 !important";>Read more <i class="fas fa-angle-double-right"></i></a>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-4">
+         <div class="card bg-light">
+            <img class="card-img-top" src="images/swiss.png" alt="Card image cap">
+            <div class="card-body">
+               <h5 class="card-title border-bottom pb-3">Swiss <a href="#" class="float-right btn btn-sm btn-info d-inline-flex share"><i class="fas fa-share-alt"></i></a></h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <a href="#" class="btn btn-sm btn-info float-right" style="background-color:red !important";>Read more <i class="fas fa-angle-double-right"></i></a>
+            </div>
+         </div>
+      </div>
+   </div>
+   
+   
 
  
-    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  
 </body>
 
 </html>
