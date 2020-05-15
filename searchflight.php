44bc237fe1b8c4486e4a83d70a74e531b2d2cb86
belindazhegrova <53189831+belindazhegrova@users.noreply.github.com>
2020-05-15 23:04:16 +0200

    commiting searchflight

---
+++ searchflight.php
@@ -0,0 +1,74 @@
+<?php
+
+include 'dbconnect.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+      <title>travel booking</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="searchflight.css">
+</head>
+<body>
+
+  <div class="card">
+    <div class="title">
+    <h1 >Search Flight</h1>
+    </div>
+
+
+           <form action="searchresult.php" method="post">
+            <div class="row">
+        <div class="from">
+      <label>From</label><br>
+
+      <input type="text" name="from" placeholder="City or Airport" ><br>
+        </div>
+
+        <div class="to">
+      <label>To</label>
+      <input type="text" name="to" placeholder="City or Airport"  >
+      </div>
+
+
+
+    <div class="row">
+      <div class="dep">
+
+      <label>Departing</label><br>
+      <input type="date" name="depart" ><br>
+
+
+    </div>
+    <br>
+
+    <div class="row">
+      <div class="class">
+
+     <label>Class</label><br>
+       <select class="txtclass"  name="class">
+        <option value="Economy" name="class">Economy</option>
+        <option value="Business" name="class"> Business</option>
+       </select>
+         </div>
+
+
+    </div>
+    <br>
+
+        <div class="submit">
+                <input style="background-color: red;" type="submit" id="butoni"  name="submit" value="Search Flight">
+                </div>
+ 
+    </div>
+    </div>
+
+    </form>
+</div>
+
+</body>
+</html>
