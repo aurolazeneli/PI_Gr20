8426abc4496f8ea2cd626c7fefd2831d433981e4
BleonaZenelaj <44550465+BleonaZenelaj@users.noreply.github.com>
2020-05-15 23:23:39 +0200

    Add files via upload

---
+++ 14.php
@@ -0,0 +1,7 @@
+<?php include "MySQL/db.php";?>
+<?php include "functions.php";?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
