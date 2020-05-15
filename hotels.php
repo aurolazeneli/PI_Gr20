171bb0fbeba6c9763a1fe58dfb57bc328c9e0a14
BleonaZenelaj <44550465+BleonaZenelaj@users.noreply.github.com>
2020-05-15 23:45:38 +0200

    Update hotels.php

--- hotels.php
+++ hotels.php
@@ -9,15 +9,9 @@
 * {
   box-sizing: border-box;
 }
-
 body {
-  
-  font-family: Arial, Helvetica, sans-serif;
+    font-family: Arial, Helvetica, sans-serif;
 }
-
-
-
-/* Style the content */
 .content {
   margin-left: 130px;
   padding-left: 7px;
@@ -25,59 +19,72 @@ body {
 </style>
 </head>
 <body>
-<?php include'header.php'?>
+    <?php
+include('header.php'); ?>
+
 <br>
-
-
-
   <div class="articles">
     <div class="box">
         <a href="Dubai.php"><img class="imgs" src="images/d3.1.jpg" alt=""></a>
         <a href="Dubai.php"><h3> Dubai </h3></a>
-        <p>Over the last decade, Dubai has seen strong growth within the tourism sector through the addition of new attractions and landmarks. From January to June 2019 8.36 million international tourists visited Dubai. Most of the visitors are from India (997.000) followed by Saudi Arabia (755.000) and the United Kingdom (586,000).
-
-            It is estimated that there will be 20 million visitors in 2020.<a href="Dubai.html">Check our hotels</a> </p>
-            <span> From <span class="authors">$500</span> /per person</span>
+        <?php
+echo readfile("dubai.txt");
+       ?> 
+            
     </div>
 </div>
 <div class="articles">
     <div class="box">
         <a href="finland.php"><img class="imgs" src="images/f6.1.webp" alt=""></a>
         <a href="finland.php"><h3> Finland</h3></a>
-            <p> During the summertime thousands of tourists approach Helsinki by cruising boats travelling across the Baltic Sea. Helsinki is known as a clean, modern and safe meeting point between the east and west. Other popular tourist destinations within Finland include Jyväskylä, Tampere, Turku, Oulu, Rovaniemi and Porvoo<a href="finland.php">Check Our Hotels</a> </p>
-            <span> From <span class="authors">$250</span> / per person</span>
+        <?php
+$myfile = fopen("finland.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("finland.txt"));
+fclose($myfile);
+?> 
     </div>
 </div>
 <div class="articles">
     <div class="box">
         <a href="greece.php"><img class="imgs" src="images/g3.1.PNG" alt=""></a>
         <a href="greece.php"><h3> Greece</h3></a>
-        <p> Ancient archaeological sites, cliffs tumbling into sparkling blue water, sand and pebble beaches, and a balmy Mediterranean climate make Greece one of Europe's prime tourist destinations. Besides the capital, Athens, top things to see on the mainland include Ancient Delphi and the monasteries of Meteora.<a href="greece.php">Read More</a> </p>
-        <span> From <span class="authors">$400</span> /per person</span>
+        
+        <?php
+echo readfile("greece.txt");
+       ?> 
     </div>
 </div>
 <div class="articles">
     <div class="box">
         <a href="Italy.php"><img class="imgs" src="images/i2.1.jpg" alt=""></a>
         <a href="Italy.php"><h3> Italy</h3></a>
-        <p> People mainly visit Italy for its rich culture, cuisine, history, fashion and art, its beautiful coastline and beaches, its mountains, and priceless ancient monuments. Italy also contains more World Heritage Sites than any other country in the world<a href="Italy.php">Check our Hotels</a> </p>
-        <span> From <span class="authors">$430</span> | /per person</span>
+        <?php
+echo readfile("italy.txt");
+       ?>
      </div>
 </div>
 <div class="articles">
     <div class="box">
         <a href="maldive.php"><img class="imgs" src="images/m5.1.jpg" alt=""></a>
         <a href="maldive.php"><h3>Maldives</h3></a>
-        <p> Tourism is the largest economic industry in the Maldives, as it plays an important role in earning foreign exchange revenues and generating employment in the tertiary sector of the country. The archipelago of the Maldives is the main source of attraction to many tourists visiting the country worldwide.<a href="maldive.php">Check Our Hotels</a> </p>
-        <span> From <span class="authors">$850</span> | /per person</span>
+        <?php
+echo readfile("maldives.txt");
+       ?>
+        
     </div>
 </div>
 <div class="articles">
     <div class="box">
         <a href="switzerland.php"><img class="imgs" src="images/s5.1.jpg" alt=""></a>
         <a href="switzerland.php"><h3>Switzerland</h3></a>
-       <p> With lakes, forests, mountains and clean air Switzerland has much to offer visitors. The tourism sector is one of the main drivers of the Swiss economy. With 35.6 million overnight stays in 2015, the Swiss tourism industry generated around 2.8% of the country’s gross domestic product, or a total of CHF 17.4 billion. More than 170,000 people (full-time-equivalents) work in the tourism industry. In 2015 guests from Germany racked up the most overnight stays Europe-wide (4 million). <a href ="switzerland.php">Check our hotels</a> </p>
-        <span> From <span class="authors">$550</span> /per person</span>
+        <?php
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "With lakes, forests, mountains and clean air Switzerland has much to offer visitors. The tourism sector is one of the main drivers of the Swiss economy. With 35.6 million overnight stays in 2015, the Swiss tourism industry generated around 2.8% of the country’s gross domestic product, or a total of CHF 17.4 billion. More than 170,000 people (full-time-equivalents) work in the tourism industry. In 2015 guests from Germany racked up the most overnight stays Europe-wide (4 million). /n";
+fwrite($myfile, $txt);
+fclose($myfile);
+echo readfile("newfile.txt");
+?>
+
      </div>
 </div>
 
