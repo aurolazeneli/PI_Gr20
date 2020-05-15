1758b67c754a2224fc709fde1a0ef80858e9dd5c
Aurola Zeneli <52915274+aurolazeneli@users.noreply.github.com>
2020-05-15 04:22:09 +0200

    updating the header

--- includes/head.php
+++ includes/head.php
@@ -1,18 +1,21 @@
 
-  <style>
-  <?php include 'main.css'; ?>
+ <style>
+<?php include 'main.css'; ?>
 
-  </style>
+</style>
 
+ <div id="main_header" style="width:100%; height:40px;   !important;" >
+    <div class="navbar-nav">
+           <a  class="nav-item nav-link " href="index.php">Home </a>
+           <a class="nav-item nav-link" href="hotels.php">Hotels</a>
+           <a class="nav-item nav-link" href="#">Flights</a>
+           <a class="nav-item nav-link " href="#">Gallery</a>
+           <a class="nav-item nav-link" href="#">Contact</a>
+           <i style="color:red; size:120%; margin-left:20%;"class="fa fa-user-circle"></i>
+           <a href="#" class="nav-item nav-link"
+           <h8 style="color:white; margin:0; padding:0" >Login</h8></a>
+      <!--  <a><i style=\"color:red; margin-left:20%;\" class=\"fa fa-user-circle-o\"></i></a>-->
+       </div>
 
-  <div id="main_header">
-         <a  class="nav-item nav-link " href="#">Home </a>
-         <a class="nav-item nav-link" href="#">Hotels</a>
-         <a class="nav-item nav-link" href="#">Flights</a>
-         <a class="nav-item nav-link " href="#">Gallery</a>
-         <a class="nav-item nav-link " href="#">Contact</a>
-         <i style="color:red; size:120%; margin-left:20%;"class="fa fa-user-circle"></i>
-         <a href="#" class="nav-item nav-link"
-         <h8 style="color:white; margin:0; padding:0" >Login</h8></a>
-    <!--  <a><i style="color:red; margin-left:20%;" class="fa fa-user-circle-o"></i></a>-->
-     </div>
+         </div>
+
