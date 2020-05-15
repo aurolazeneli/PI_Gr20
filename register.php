adc7314cddc11c978747b88d7ec25b8a19cb230b
BleonaKonjuhi <52915332+BleonaKonjuhi@users.noreply.github.com>
2020-05-15 23:09:06 +0200

    Update register.php

--- register.php
+++ register.php
@@ -5,6 +5,8 @@
     <title>
      Login
     </title>
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -17,6 +19,7 @@
 
 </head>
 <body>
+	     <?php include "includes/head.php" ?>
 
 	
     <div class="login-reg-panel">
@@ -58,4 +61,6 @@
 	</div>
 
     </body>
+    
+
 </html>
