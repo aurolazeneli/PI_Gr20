946f5ac30f5be5ef54ce113418e85af5d1cc4057
BleonaKonjuhi <52915332+BleonaKonjuhi@users.noreply.github.com>
2020-05-17 21:36:54 +0200

    Update greece.php

--- greece.php
+++ greece.php
@@ -3,11 +3,13 @@
 <head>
 <title>Greece</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="2.css">
 </head>
 
 <body>
-<?php include'include/head.php'?>
+<?php include'includes/head.php'?>
   <i><h1 style="text-align:center;text-emphasis-style: 122;
     text-shadow:1px 1px red;"> Greece Hotels</h1> </i>
 <div class="content display-container">
