724d483fc704de345490580f027d522b22794d50
belindazhegrova <53189831+belindazhegrova@users.noreply.github.com>
2020-05-17 05:05:32 +0200

    commiting new files

---
+++ greece.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Greece</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="2.css">
+</head>
+
+<body>
+<?php include'include/head.php'?>
+  <i><h1 style="text-align:center;text-emphasis-style: 122;
+    text-shadow:1px 1px red;"> Greece Hotels</h1> </i>
+<div class="content display-container">
+  <img class="mySlides" src="images/g1.1.PNG" style="width:100%">
+  <img class="mySlides" src="images/g2.1.PNG" style="width:100%">
+  <img class="mySlides" src="images/g3.1.PNG" style="width:100%">
+  <img class="mySlides" src="images/g4.1.PNG" style="width:100%">
+  <img class="mySlides" src="images/g5.1.PNG" style="width:100%">
+  <img class="mySlides" src="images/g6.1.PNG" style="width:100%">
+
+</div>
+
+
+
+<div class="row">
+  
+        <div class="column">
+          <a href="13.php">
+<div class="card">
+    
+        <img src="images/g1.1.PNG" alt="Greece Hotel" style="width:100%">
+        <p1 style="font-size:19px;
+        text-shadow:1px 1px red;">Volcano Santorini</p1>
+        <p class="price">$132</p>
+        <p1>Popular with guest from US</p1>
+    </div>
+</a>
+        </div>
+        
+
+        
+        <div class="column">
+          <a href="14.php">
+      <div class="card" >
+            <img src="images/g2.1.PNG" alt="Greece Hotel" style="width:100%">
+            <p1 style="font-size:19px;
+        text-shadow:1px 1px red;">Sunset Paros</p1>
+            <p class="price">$680</p>
+            <p1> High-end beach hotel</p1>
+            
+     </div>
+     </a>
+        </div>
+
+        <div class="column">
+          <a href="15.php">
+        <div class="card" >
+            <img src="images/g3.1.PNG" alt="Greece Hotel nr.3" style="width:100%">
+            <p1 style="font-size:19px;
+        text-shadow:1px 1px red;">Mykonos Princess</p1>
+            <p class="price">$100</p>
+            <p1>Offering a polished restaurant </p1>
+            
+     </div>
+</a>
+        </div>
+
+
+    </div>
+    <br>
+    <br>
+    <div class="row2">
+            <div class="column">
+              <a href="images/greece4.html">
+           <div class="card">
+            <img src="16.php" alt="Greece Hotel" style="width:100%">
+            <p1 style="font-size:19px;
+        text-shadow:1px 1px red;">Royal Hotel</p1>
+            <p class="price">$308</p>
+            <p1>Elegant Rooms and suits </p1>
+            
+        </div>
+        </a>
+            </div>
+    
+            
+            <div class="column">
+              <a href="17.php">
+          <div class="card" >
+                <img src="images/g5.1.PNG" alt="Greece Hotel" style="width:100%">
+                <p1 style="font-size:19px;
+        text-shadow:1px 1px red;">Avaton Luxory</p1>
+                <p class="price">$200</p>
+                <p1>Luxe suits with sea viws</p1>
+                
+         </div>
+         </a>
+            </div>
+    
+            <div class="column">
+              <a href="18.php">
+            <div class="card" >
+                <img src="images/g6.1.PNG" alt="Greece Hotel" style="width:100%">
+                <p1 style="font-size:19px;
+        text-shadow:1px 1px red;">Diana Palace </p1>
+                <p class="price">$600</p>
+                <p1>Down to earth hotel</p1>
+                
+         </div>
+    </a>
+            </div>
+    
+         
+         </div>
+         <?php include'includes/footer.php'?>
+         </body>
+         </html>
