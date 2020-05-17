6621c56021cd5582b385b2c566c3e4d5ab0bb93c
belindazhegrova <53189831+belindazhegrova@users.noreply.github.com>
2020-05-17 07:23:49 +0200

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -63,10 +63,13 @@ if(!$mail->Send()) {
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <link rel="stylesheet" type="text/css" href="contactus.css">
+
+ <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="main.css">
 <!------ Include the above in your HEAD tag ---------->
 </head>
-    <header>
-</header>
+
 
 
 <div class="container contact">
