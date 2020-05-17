1f4fa877fab8b3b64d7d0b9f2fc4d0b877a68986
Aurola Zeneli <52915274+aurolazeneli@users.noreply.github.com>
2020-05-17 20:48:26 +0200

    Update 3.php

--- 3.php
+++ 3.php
@@ -77,6 +77,7 @@ function showSlides(n) {
  <tr>
    <th><i>General Facilities </i><span> <hr> </span> </th>
    <th><i>Rating</i> <span> <hr> </span></th>
+      <th ><i style="padding-left:200px">Booking </i><span> <hr> </span> </th>
 
  </tr>
  <tr>
