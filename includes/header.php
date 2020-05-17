bcb6e092df9947149b1a9f365188d11f11349cc6
Aurola Zeneli <52915274+aurolazeneli@users.noreply.github.com>
2020-05-17 05:14:51 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
 document.getElementById("main_header").style.top = "0";
 } else{
 document.getElementById("main_header").style.top = "-50px";
@@ -15,32 +15,35 @@ document.getElementById("main_header").style.top = "-50px";
 </script>
 
 <?php
- echo "<div id=\"main_header\" style=\"width:100%; height:40px;   top:-50px !important;\" >
+ echo "<div id=\"main_header\" style=\"width:100%; height:50px; padding-left:310px;  top:-50px !important;\" >
     <div class=\"navbar-nav\">
-           <a  class=\"nav-item nav-link \" href=\"#\">Home </a>
-           <a class=\"nav-item nav-link\" href=\"#\">Hotels</a>
-           <a class=\"nav-item nav-link\" href=\"#\">Flights</a>
-           <a class=\"nav-item nav-link \" href=\"#\">Gallery</a>
-           <a class=\"nav-item nav-link\" href=\"#\">Contact</a>
+         <img src=\"images/logo1.png\" alt=\"\" style=\"width:8%; height:200%; margin-top:-25px;\">
+           <a  class=\"nav-item nav-link \" href=\"index.php\">Home </a>
+           <a class=\"nav-item nav-link\" href=\"hotels.php\">Hotels</a>
+           <a class=\"nav-item nav-link\" href=\"searchflight.php\">Flights</a>
+           <a class=\"nav-item nav-link \" href=\"gallery.php\">Gallery</a>
+           <a class=\"nav-item nav-link\" href=\"contactus.php\">Contact</a>
            <i style=\"color:red; size:120%; margin-left:20%;\"class=\"fa fa-user-circle\"></i>
-           <a href=\"#\" class=\"nav-item nav-link\"
+           <a href=\"login.php\" class=\"nav-item nav-link\"
            <h8 style=\"color:white; margin:0; padding:0\" >Login</h8></a>
       <!--  <a><i style=\"color:red; margin-left:20%;\" class=\"fa fa-user-circle-o\"></i></a>-->
        </div>
 
          </div>";
+        //<img src=\"images/logo.png\" alt=\"\" style=\"width60px; height:60px;\">
 ?>
 
 
-<div class="navbar-nav">
-       <a  class="nav-item nav-link " href="#">Home </a>
-       <a class="nav-item nav-link" href="#">Hotels</a>
-       <a class="nav-item nav-link" href="#">Flights</a>
-       <a class="nav-item nav-link " href="#">Gallery</a>
-       <a class="nav-item nav-link " href="#">Contact</a>
+<div class="navbar-nav" style="padding-left:-60px;">
+     <img src="images/logo1.png" alt="" style=" width:10%; height:100%; margin-top:-25px;">
+       <a  class="nav-item nav-link " href="index.php">Home </a>
+       <a class="nav-item nav-link" href="hotels.php">Hotels</a>
+       <a class="nav-item nav-link" href="searchflight.php">Flights</a>
+       <a class="nav-item nav-link " href="gallery.php">Gallery</a>
+       <a class="nav-item nav-link " href="contactus.php">Contact</a>
        <i style="color:red; size:120%; margin-left:20%;"class="fa fa-user-circle"></i>
-       <a href="#" class="nav-item nav-link"
+       <a href="login.php" class="nav-item nav-link"
        <h8 style="color:white; margin:0; padding:0" >Login</h8></a>
   <!--  <a><i style="color:red; margin-left:20%;" class="fa fa-user-circle-o"></i></a>-->
    </div>
-   
+
