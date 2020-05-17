414e7cb075675664ef25c9e3f5c24754e20a81c8
Aurola Zeneli <52915274+aurolazeneli@users.noreply.github.com>
2020-05-17 21:38:44 +0200

    Update switzerland.php

--- switzerland.php
+++ switzerland.php
@@ -4,10 +4,13 @@
 <title>Switzerland</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="2.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
-<?php include'include/head.php'?>
+<?php include "includes/head.php"?>
   <i><h1 style="text-align:center;text-emphasis-style: 122;
     text-shadow:1px 1px red;"> Switzerland Hotels</h1> </i>
 <div class="content display-container">
@@ -112,6 +115,6 @@
     
          
          </div>
-         <?php include'include/footer.php'?>
+         <?php include "includes/footer.php"?>
          </body>
          </html>
