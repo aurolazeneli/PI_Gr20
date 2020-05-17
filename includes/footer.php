d78d250965904aa03fee367c8038a1d89d6374e3
Aurola Zeneli <52915274+aurolazeneli@users.noreply.github.com>
2020-05-17 05:34:26 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,43 +1,34 @@
-<footer>
-  <div class="foot" style="background-color:#090736; width: 100%; padding-top:7px; padding-left: 20%">
-  <p style="color: white; padding-top: 5px;padding-bottom: 5px; font-size: 0.8rem; text-align: center">  You can be informed of all our opportunities! </p>
+<footer style="width:100%">
+  <div class="foot" style="background-color:#090736; width: 100%;height:40px; padding-top:7px; padding-left: 20%">
+  <!--<p style="color: white; padding-top: 5px;padding-bottom: 5px; font-size: 0.8rem; text-align: center">  You can be informed of all our opportunities! </p>
   <div class="input-group mb-3" style="width: 360px; padding-left: 5px;">
-  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Email" aria-describedby="basic-addon2">
+<input type="text" class="form-control" placeholder="Recipient's username" aria-label="Email" aria-describedby="basic-addon2">
   <div class="input-group-append">
-    <span class="input-group-text" id="basic-addon2">Sign Up!</span>
+    <span class="input-group-text" id="basic-addon2">Sign Up!</span>-->
   </div>
 </div>
 
   </div>
   <div class="row3">
+
     <div class="column3">
-    <p>  © 2018 THE COPYRIGHT OF ALL THE
-      VISUAL MATERIALS AND ALL CONTENTS
-      IN THE SITE BELONG TO TRAVELTIME.</p>
-    </div>
-    <div class="column3">
-    <ul>
-        <a href="#" style="  text-decoration: none !important; color:black"> <li>HOTEL</li></a>
-        <a href="#"><li>FLIGHTS</li></a>
-        <a href="#">  <li>GALLERY</li></a>
-       <a href="#">  <li>CONTACT</li></a>
+    <ul style="padding-top:50px">
+        <a href="hotels.php" style="  text-decoration: none !important; color:black"> <li>HOTEL</li></a>
+        <a href="searchflight.php"><li>FLIGHTS</li></a>
+        <a href="gallery.php">  <li>GALLERY</li></a>
+       <a href="contactus.php">  <li>CONTACT</li></a>
     </ol>
     </div>
     <div class="column3">
-<ul>
-  <a href="#">  <li>PARIS</li></a>
-    <a href="#"><li>LONDON</li></a>
-    <a href="#"><li>MALDIVE</li></a>
-    <a href="#"><li>ROME</li></a>
+<ul style="padding-top:50px">
+  <a href="switzerland.php">  <li>SWITZERLAND</li></a>
+    <a href="Italy.php"><li>ITALY</li></a>
+    <a href="Dubai.php"><li>DUBAI</li></a>
+    <a href="maldive.php"><li>MALDIVE</li></a>
 </ol>
     </div>
-    <div class="column3">
-    <ul>
-        <a href="#"><li>SERVICE</li></a>
-        <a href="#"><li>SERVICE</li></a>
-        <a href="#"><li>SERVICE</li></a>
-        <a href="#"><li>SERVOCE</li></a>
-    </ol>
+    <div class="column3" style="padding-top:120px !important;">
+<a href="weather.php" ><button type="button" class="btn btn-danger" name="button">Weather API</button> </a>
     </div>
 
   </div>
@@ -54,12 +45,13 @@
   </style>
   <div class="last" >
 
-   <a href="index.php" >   <button type="button" name="button" id="up" style=" background-color:#616161;"><i style="color:white;" class="fa fa-arrow-up"></i></button></a>
+
   </div>
 </footer>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
